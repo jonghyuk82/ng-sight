d63e3fe841f78bc265cc775497a378c6c5faeaf1
jonghyuk82 <jonghyuk82@gmail.com>
2021-09-01 22:26:41 -0400

    Fixed fetch data via API on sales-data.service.cs

--- src/app/services/sales-data.service.ts
+++ src/app/services/sales-data.service.ts
@@ -6,15 +6,13 @@ import { Order } from '../shared/order';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SalesDataService {
+  baseUrl = 'http://localhost:5000/api/order/';
+  constructor(private _http: HttpClient) {}
 
-  baseUrl = 'http://localhost:5000/api/';
-  constructor(private _http: HttpClient) { }
-  
-  getOrders(pageIndex: number, pageSize: number) : Observable<Order[]>
-  {
-    return this._http.get<Order[]>(this.baseUrl + pageIndex + '/' + pageSize);    
+  getOrders(pageIndex: number, pageSize: number): Observable<Order[]> {
+    return this._http.get<Order[]>(this.baseUrl + pageIndex + '/' + pageSize);
   }
 }
