72315e3263d80cefd243755708f172810fdd7691
jonghyuk82 <jonghyuk82@gmail.com>
2021-09-04 11:12:20 -0400

    Completed pagination

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -6,10 +6,9 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
-  styleUrls: ['./section-orders.component.css']
+  styleUrls: ['./section-orders.component.css'],
 })
 export class SectionOrdersComponent implements OnInit {
-
   // constructor() { }
 
   // orders: Order[] = [
@@ -40,45 +39,41 @@ export class SectionOrdersComponent implements OnInit {
   //   },
   // ];
 
-  constructor(private _salesData: SalesDataService) { }
-  
+  constructor(private _salesData: SalesDataService) {}
 
   orders: Order[];
   total = 0;
   page = 1;
   limit = 10;
   loading = false;
-  
 
-  ngOnInit(): void
-  {
-    this.getOrders();    
+  ngOnInit(): void {
+    this.getOrders();
   }
 
-  getOrders(): void
-  {
-    this._salesData.getOrders(this.page, this.limit)
-      .subscribe((res) =>
-      {
-        console.log('Result from getOrders: ', res);
-        this.orders = res['page']['data'];
-        this.total = res['page']['total'];
-        this.loading = false;        
-      });
+  getOrders(): void {
+    this._salesData.getOrders(this.page, this.limit).subscribe((res) => {
+      console.log('Result from getOrders: ', res);
+      this.orders = res['page']['data'];
+      this.total = res['page']['total'];
+      this.loading = false;
+    });
   }
 
-  goToPrevious(): void
-  {
+  goToPrevious(): void {
     //console.log('Previous button clicked');
     this.page--;
     this.getOrders();
   }
 
-  goToNext(): void
-  {
+  goToNext(): void {
     //console.log('Next button clicked');
     this.page++;
     this.getOrders();
   }
 
+  goToPage(n: number): void {
+    this.page = n;
+    this.getOrders();
+  }
 }
